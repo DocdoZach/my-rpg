a94df72bb35f56ef8098396132bd1d9717262668
DocdoZach <zach.kaos@gmail.com>
2024-12-08 18:38:16 -0500

    tried to make a character

--- main.py
+++ main.py
@@ -5,24 +5,39 @@ Zach N
 
 import pygame
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, image_file):
+        super().__init__()
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Game window
+screen_x = 800
+screen_y = 600
+screen = pygame.display.set_mode((screen_x, screen_y))
 
-player = pygame.Rect((388, 280, 12, 20))
+# Player
+pos_x = 400
+pos_y = 300
+player = Entity(pos_x, pos_y, "media/sprites/doc.png")
+sprite_group = pygame.sprite.Group()
+sprite_group.add(player)
 
+# Game loop
 run = True
 while run:
-
-    pygame.draw.rect(screen, (0, 0, 255), player)
-
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
+    screen.fill((255, 255, 255))
+    sprite_group.draw(screen)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] == True:
+        pos_x += 1
+
+    pygame.display.flip()
 
 pygame.quit()
